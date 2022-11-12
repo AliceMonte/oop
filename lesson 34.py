497eeca71bd8ab5e7a4a21754acf59a094ea264b
AliceMonte <lol.gerda96@gmail.com>
2022-11-12 19:28:18 +0300

    lesson 34

---
+++ lesson 34.py
@@ -0,0 +1,29 @@
+#Добавить абстрактный класс Animal и переопределить в нём все будущие абстрактные методы.
+from abc import abstractmethod
+class Animal:
+    def __init__(self):
+        pass
+    def clasvid(self):
+        pass
+    def agepopulation(self):
+        pass
+    def rar(self):
+        pass
+class Fish(Animal):
+    def __init__(self, clas, vid, age, population, rare):
+        self.clas = clas
+        self.vid = vid
+        self.age = age
+        self.population = population
+        self.rare = rare
+    def clasvid(self):
+        print('Класс:', self.clas, ',', 'Вид: ', self.vid)
+    def agepopulation(self):
+        print('Возраст: ', self.age,',', 'Популяция в мире составляет: ', self.population)
+    def rar(self):
+        print('Редкость: ', self.rare)
+
+animal = Fish('лучепёрые рыбы', 'скумбрия', '1 год', 'не установлено', 'часто')
+animal.clasvid()
+animal.agepopulation()
+animal.rar()
